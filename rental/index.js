1511704e2a673c141b712c1bcf225879d101a1e3
Yullo Costa Bomfim <35705562+yullobomfim@users.noreply.github.com>
2022-05-18 07:45:59 -0300

    Add files via upload

---
+++ rental/index.js
@@ -0,0 +1,47 @@
+import express from 'express'
+const app = express();
+import axios from 'axios'
+
+app.use(express.json())
+
+const URL_SERVICO_1 = 'http://localhost:9001'
+const URL_SERVICO_2 = 'http://localhost:9002'
+
+async function getServices(url) {
+    try {
+        let res = await axios({
+            url: url,
+            method: 'get',
+            timeout: 5000,
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-type': 'Access-Control-Allow-Methods'
+            }
+        })
+        return res.data
+    }
+    catch (error) {
+        console.log(error)
+    }
+  }
+  
+async function services(url) {
+    
+    const a = await getServices(url)
+        var array = []  
+        a.map((item)=>{
+            if (item.VeiculoDisponivel==true){
+                array.push(item)
+            }
+        })
+        console.log(array)
+
+    return array
+
+} 
+
+
+//   const users = services(URL_SERVICO_1)
+    const vehicles = services(URL_SERVICO_2)
+//   console.log(users)
+    console.log(vehicles)
